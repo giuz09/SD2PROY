15438c1ef77324233537640331ec5c882f0caed0
Unknown <liamoreno14@gmail.com>
2018-11-19 18:44:20 -0300

    Lunes 19-11 19hs
    funciona todo en general. ver gather sin que tome le hilo 0, hacer prueba de escritorio de calculos, no esta en general

--- src/MPIMultMatriz2.java
+++ src/MPIMultMatriz2.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Random;
 
 import mpi.Datatype;
@@ -5,114 +6,184 @@ import mpi.MPI;
 
 public class MPIMultMatriz2 {
 
+	static int N=2;
+	static int M=4;	
+	static Object envio[]=new Object[2];
+	static double  vectorBN [] = new double[M];
+	static double vectorCompletoC [] = new double[6];
+	static double vectorSubC[] = new double[N];
+	static double vectorCompletoD [] = new double[6];
+	static double vectorSubD[] = new double[N];
+	static double matrizGigante[][]=new double[M][M];
+	static double subMatriz1[][] = new double[N][M]; //original
+	static double subMatriz2[][] = new double[N][M]; 	
+	static double subMatrizAux[][] = new double[N][M];
+	static double matrizFinal[][] = new double[M][M];
+	static Object objetoFinal=new Object();
+	static double sumatoria = 0.0;
+	static int me,size;
+	
+
+	
+	public static void dividoMatriz(double [][] matrizGigante,Integer M,double [][] subM1,double [][] subM2 ) {
+		Integer N = (M/2);
+		inicializoMatrizAleatorios(matrizGigante); 
+		System.out.println("MATRIZ INICIAL  A ");
+		muestroMatrizG(matrizGigante);
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < matrizGigante.length; j++) {
+				subM1[i][j]= matrizGigante[i][j];
+			}
+		}
+		for (int i = 2; i < M; i++) {
+			for (int j = 0; j < matrizGigante.length; j++) {
+				subM2[i-2][j]= matrizGigante[i][j];
+			}
+		}
+	}	
+	
+	public static double[] multiMatrizVector(double[][] matriz,double[] vector) {
+		
+		
+		 double  vectorResus [] = new double[N];
+		inicializoVector00(vectorResus);
+		////////metodo que mutiplica matriz por vector
+		for (int i = 0; i < N; i++) {
+			sumatoria = 0.0;
+			for (int j = 0; j < M; j++) {	
+				
+			  sumatoria = (sumatoria +matriz[i][j]*vector[j]);
+			}
+			vectorResus[i] = sumatoria;
+		}	
+			
+			return vectorResus;
+	}
+	
+	public static double productoVectores(double[] vectorA, double[] vectorB) {
+		double resu =0.00;
+		for (int i = 0; i < vectorB.length; i++) {
+			resu = resu + (vectorA[i]*vectorB[i]);
+		}
+		return resu;
+	}
+	
+	public static void muestrovector(double[] vector) {
+		for (int i = 0; i < vector.length; i++) {
+			System.out.println("	"+i+"----"+vector[i]);
+		}	
+		System.out.println(".........................................................");
+	}
+	
+	public static void muestroMatriz(double[][]matriz) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				System.out.print("	"+matriz[i][j]);
+			}
+			System.out.println(" ");
+		}	
+		
+	}	
+	public static void muestroMatrizG(double[][]matriz) {
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < M; j++) {
+				System.out.print("	"+matriz[i][j]);
+			}
+			System.out.println(" ");
+		}	
+		
+	}	
+	
+	public static void inicializoMatrizAleatorios(double[][] matriz) {
+		
+		Random r = new Random();
+		for(int i=0;i<M;i++)
+		{
+			for(int j=0;j<M;j++)
+			{
+				matriz[i][j]=r.nextDouble();	
+			}				
+		}	
+	}
+	
+	public static void inicializoVectorAleatorios(double[] vector) {
+		Random r = new Random();
+		for(int i=0;i<vector.length;i++)
+		{				
+			vector[i]=r.nextDouble();
+		}	
+	}
+	
+	public static void inicializoVector00(double [] vector) {
+		for (int i = 0; i < vector.length; i++) {
+			vector[i] =0.0;
+		}
+	}
 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub
-		int me,size;
+		
 		args = MPI.Init(args);
 		me = MPI.COMM_WORLD.Rank();
 		size = MPI.COMM_WORLD.Size();
-		int N=2;
-		int M=4;
 		
-		
-		Object envio[]=new Object[2];
-		double  vectorBN [] = new double[M];
-		double vectorCompletoCN [] = new double[6];
-		double vectorResultado[] = new double[N];
-		
-		double subMatriz1[][] = new double[N][M]; //original
-		double subMatriz2[][] = new double[N][M]; 
-		
-		double subMatrizAux[][] = new double[N][M];
-		double matrizFinal[][] = new double[M][M];
-		Object objetoFinal=new Object();
-		
-		double sumatoria = 0.0;
-		
-		
-		///////////////////////////// maestro /////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////////////
 		if(me==0)
 		{
 			
-				Random r = new Random();
-				for(int i=0;i<N;i++)
-				{
-					for(int j=0;j<M;j++)
-					{
-						subMatriz1[i][j]=r.nextDouble();
-						subMatriz2[i][j]=r.nextDouble();	
-					}				
-					vectorBN[i]=r.nextDouble();
-				}
-					
+				//inicializoMatrizAleatorios(subMatriz1); 	
+				//inicializoMatrizAleatorios(subMatriz2);
+				dividoMatriz(matrizGigante, M, subMatriz1, subMatriz2);
+				inicializoVectorAleatorios(vectorBN); 
+				inicializoVector00(vectorCompletoC);
+				inicializoVector00(vectorSubC);
+				inicializoVector00(vectorCompletoD);
+				inicializoVector00(vectorSubD);
 				envio[0]=(Object)subMatriz1;
-				envio[1]=(Object)subMatriz2;
+				envio[1]=(Object)subMatriz2; 
 				
-				for (int i = 0; i <= 1; i++) {					
-						MPI.COMM_WORLD.Send(envio, i,1, MPI.OBJECT, i+1 , 10);  // manda desde la pos 0, 1 objeto, a partir del hilo 1 al 3
-						System.out.println("soy el maestro");				
-				}		
-				System.out.println("Finaliza matriz enviada por el hilo: " + me);
-				
-				for (int i = 0; i < vectorCompletoCN.length; i++) {
-					vectorCompletoCN[i] =0.0;
-				}
-				for (int i = 0; i < vectorResultado.length; i++) {
-					vectorResultado[i] =0.0;
-				}
+				for (int i = 0; i <= 1; i++) {
+					MPI.COMM_WORLD.Send(envio, i,1, MPI.OBJECT, i+1 , 10);}	 
+				System.out.println("Matriz sub1"); muestroMatriz(subMatriz1);
+				System.out.println("Matriz sub2"); muestroMatriz(subMatriz2);
+				System.out.println("vector B");muestrovector(vectorBN);				
 		}
 		
-		
-		///////////////////////////////////////////////////////////////////////////////////////
-		
-		if (me!=0)
-		{
-				Object recepcion[]=new Object[2];
-				MPI.COMM_WORLD.Recv(recepcion,0, 1, MPI.OBJECT, 0,10);	
-				System.out.println(recepcion.toString());
-				
-				subMatrizAux=(double[][])  recepcion[0];		
-				System.out.println("Finaliza matriz recibida por el hilo: " + me);
-						
-		}
-		
-		//////////////////////////////////////////////////////////////////////////////////////////
-		MPI.COMM_WORLD.Bcast(vectorBN, 0, M, MPI.DOUBLE, 0); //envia vector a los demas hilos
-		/////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////	
 		if(me!=0) {
-				for (int i = 0; i < N; i++) {
-					sumatoria = 0.0;
-					for (int j = 0; j < M; j++) {	
-						
-					  sumatoria = (sumatoria +subMatrizAux[i][j]*vectorBN[j]);
-					}
-					System.out.println("sumatoria = "+sumatoria+" hilo "+me);
-					vectorResultado[i] = sumatoria;
-				//	System.out.println(vectorResultado[i]);
-				}	
-					for (int i = 0; i < vectorResultado.length; i++) {
-						System.out.println(" i:"+i+"vector resultador"+vectorResultado[i]+"hilo "+me);
-					}				
+		Object recepcion[]=new Object[2];
+		MPI.COMM_WORLD.Recv(recepcion,0, 1, MPI.OBJECT, 0,10);	
+		subMatrizAux=(double[][]) recepcion[0];	
+		
 		}
-
-		///////////////////////////////////////////////////////////////////////////////////////////////
-		
-			MPI.COMM_WORLD.Gather(vectorResultado, 0,vectorResultado.length , MPI.DOUBLE ,vectorCompletoCN, 0, vectorResultado.length, MPI.DOUBLE, 0); //vectorCompleto = B(M)
-
-		
-		
-		System.out.println("despues del gather"+me);
-		
-		
-		///////////////////////////////////////////////////////////////////////////////////////////////
-		if(me ==0) {
 			
-			for (int i = 0; i < vectorCompletoCN.length; i++) {
-				System.out.println("vector completo "+vectorCompletoCN[i]);
-			}
+		////////////////////////////////////////////OBTENCION DE VECTOR C//////////////////////////////////////////////
+		MPI.COMM_WORLD.Bcast(vectorBN, 0, M, MPI.DOUBLE, 0); //envia vector a los demas hilos
+			
+		if(me!=0) {vectorSubC = multiMatrizVector(subMatrizAux,vectorBN);} //cada hilo multiplica su submatriz por el vector
+		
+		MPI.COMM_WORLD.Gather(vectorSubC, 0,vectorSubC.length , MPI.DOUBLE ,vectorCompletoC, 0, vectorSubC.length, MPI.DOUBLE, 0); //vectorCompleto = B(M)	
+		
+		if(me==0) {
+			System.out.println("Vector C(N) = A(N x N) x B(N) ");
+			muestrovector(vectorCompletoC);}
+		////////////////////////////////////////////////OBTENCION DEL VECTOR D///////////////////////////////////////////////
+		
+		MPI.COMM_WORLD.Bcast(vectorCompletoC, 0, M, MPI.DOUBLE, 0); //envia vector C a todos
+		
+		if(me!=0) {vectorSubD = multiMatrizVector(subMatrizAux,vectorCompletoC);}
+		
+		MPI.COMM_WORLD.Gather(vectorSubD, 0,vectorSubD.length , MPI.DOUBLE ,vectorCompletoD, 0, vectorSubD.length, MPI.DOUBLE, 0); //vectorCompleto = B(M)	
+		
+		if (me==0) {
+			System.out.println("Vector D(N) = A(NxN) × C(N)\r\n" + 
+					"");
+			muestrovector(vectorCompletoD);
+			System.out.println("X = C(N) x D(N)= ");
+			System.out.println("		"+productoVectores(vectorCompletoC, vectorCompletoD));
+			
 		}
-		////////////////////////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////OBTENGO C * D //////////////////////////////////////////////
 		
 		
 	} //llave main
